b84ac06875129b5ff316735b51a9f3501a7c07ab
Chris Roberson <cdroberson@gmail.com>
2019-07-28 13:30:13 -0600

    Updated.

--- install/bootstrap_new_mac.sh
+++ install/bootstrap_new_mac.sh
@@ -3,6 +3,8 @@
 # Run this on a stock Mac to bootstrap it with Nate's dotfiles and customizations
 #
 
+PROJECTS="~/Projects"
+
 # Ask for the administrator password upfront
 echo "Asking for your sudo password upfront..."
 sudo -v
@@ -15,15 +17,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew doctor
 
 # Create our code directory
-[[ -d ~/src ]] || mkdir ~/src
-
-# Grab PowerLevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git  ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-# Get Homesick for dotfiles
-sudo gem install homesick --no-doc --no-ri
-homesick clone cdrobey/dotfiles
-homesick symlink dotfiles
+[[ -d "$PROJECTS" ]] || mkdir "$PROJECTS"
 
 # Install HomeBrew apps
 brew bundle --file=~/.homesick/repos/dotfiles/Brewfile
@@ -34,26 +28,31 @@ brew pin ruby
 brew pin ruby-build
 brew pin rbenv
 
+
+# Get Homesick for dotfiles
+homeshick clone cdrobey/dotfiles-new
+homeshick symlink dotfiles-new
+
 # Install some Puppet and ruby tools
-gem install r10k puppet-lint rubocop
+#gem install r10k puppet-lint rubocop
 
 # Install vim Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Get Vim plugins
-vim +PlugInstall +qall
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+## Get Vim plugins
+#vim +PlugInstall +qall
 
-# Install Oh My ZSH and change shell to zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-echo "Changing ${USER}'s shell to Brew's zsh..."
+## Install Oh My ZSH and change shell to zsh
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#echo "Changing ${USER}'s shell to Brew's zsh..."
 sudo dscl . -create "/Users/$USER" UserShell /usr/local/bin/zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Get fonts
-echo "Downloading Inconsolata fonts to ~/Library/Fonts/"
-wget -P ~/Library/Fonts/ https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/Inconsolata%2BAwesome.ttf
-wget -P ~/Library/Fonts/ https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf
-
+#echo "Downloading Inconsolata fonts to ~/Library/Fonts/"
+#wget -P ~/Library/Fonts/ https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/Inconsolata%2BAwesome.ttf
+#wget -P ~/Library/Fonts/ https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf
+#
 # Get iTerm gruvbox colors
 echo "Installing GruvBox colors for iTerm2"
 wget https://github.com/morhetz/gruvbox-contrib/raw/master/iterm2/gruvbox-dark.itermcolors
@@ -61,11 +60,11 @@ open gruvbox-dark.itermcolors
 rm gruvbox-dark.itermcolors
 
 # Run MACOS config script
-echo "Configuring a bunch of MACOS things"
-sh ~/.homesick/repos/dotfiles/home/.bin/macos.sh
-
-echo
-echo "Finished!"
+#echo "Configuring a bunch of MACOS things"
+#sh ~/.homesick/repos/dotfiles/home/.bin/macos.sh
+#
+#echo
+#echo "Finished!"
 echo
 echo "All that's left is to configure iTerm2: https://github.com/chris.roberson/dotfiles#colors-and-fonts"
 echo
